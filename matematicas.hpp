a545f91a6236e20f0874fcd6c4e0d90360cb6e0f
mjgalindo <mjgalindoramos@gmail.com>
2017-03-15 17:21:55 +0100

    Aleatoriza cada principio. Por algun motivo el random device da siempre la misma semilla

--- matematicas.hpp
+++ matematicas.hpp
@@ -4,36 +4,36 @@
 #include <cmath>
 #include <random>
 #include <SFML/Graphics.hpp>
+#include <time.h>
 
 static constexpr double PI = 3.14159265358979323846;
 
-static std::random_device generadorAleatorios;
-static std::mt19937 enredadorDeMersenne(generadorAleatorios());
+static std::mt19937 enredadorDeMersenne{(unsigned long) time(NULL)};
 
-static float anguloAleatorio()
-{
-    static std::uniform_real_distribution<float> distribucionRadianes((float)-PI, (float)PI);
+
+static float anguloAleatorio() {
+    static std::uniform_real_distribution<float> distribucionRadianes((float) -PI, (float) PI);
     return distribucionRadianes(enredadorDeMersenne);
 }
 
-static int enteroAleatorio(int minimo, int maximo){
+static int enteroAleatorio(int minimo, int maximo) {
     std::uniform_int_distribution<int> distribucionTemporal(minimo, maximo);
     return distribucionTemporal(enredadorDeMersenne);
 }
 
-static float valorAleatorio(float minimo = 0.0f, float maximo = 1.0f){
+static float valorAleatorio(float minimo = 0.0f, float maximo = 1.0f) {
     std::uniform_real_distribution<float> distribucionReales(minimo, maximo);
     return distribucionReales(enredadorDeMersenne);
 }
 
-template <typename T>
-static T distanciaEuclidea(sf::Vector2<T> a, sf::Vector2<T> b){
+template<typename T>
+static T distanciaEuclidea(sf::Vector2<T> a, sf::Vector2<T> b) {
     return sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
 }
 
-template <typename T>
-static int rad2deg(T radianes){
-    return (int)((radianes < 0 ? radianes + 2 * PI : radianes) * 180 / PI);
+template<typename T>
+static int rad2deg(T radianes) {
+    return (int) ((radianes < 0 ? radianes + 2 * PI : radianes) * 180 / PI);
 }
 
 #endif //AST3ROIDS_MATEMATICAS_HPP
