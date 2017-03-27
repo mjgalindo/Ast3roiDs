f1069f8178dba68c61e43c00145fea8cf9f8112c
Grexoj <george95zgz@gmail.com>
2017-03-27 21:23:13 +0200

    Modificaciones de la red neuronal. Creado fichero para la red con geneticos.

--- Utiles/geneticos.cpp
+++ Utiles/geneticos.cpp
@@ -11,60 +11,13 @@ sf::Vector2u resolucion = {800, 600};
 vector<float> direcciones = {PI * 3 / 4,PI,-PI * 3 / 4,PI / 2,PI / 4,0,-PI / 4,-PI / 2};
 vector<float> salidas = {-3 * (2.0 / 8.0),-2 * (2.0 / 8.0),-1 * (2.0 / 8.0),0,(2.0 / 8.0),
                          2 * (2.0 / 8.0),3 * (2.0 / 8.0),4 * (2.0 / 8.0)};
+vector<double> estadoAnterior;
+
+// Inicializa una red neuronal nueva
+neural::Network red(6, 1, {20,20,20});
+
 
 vector<Asteroide> asteroides;
-
-vector<double> direccionSegura(sf::CircleShape ovni,sf::Vector2f posicionSegura, double direccionObtenida) {
-    float vMax = 3.0f;
-    float radioPeligro = 30.0f;
-    vector<double> direcciones;
-    for(int i = 0; i < direcciones.size(); i++) {
-        ovni.setPosition(posicionSegura);
-        bool choque = false;
-        float distanciaRecorrida = 0.0f;
-        while(distanciaRecorrida < radioPeligro && !choque) {
-            //MOVER OVNI Y COMPROBAR QUE CHOCA
-            ovni.move({vMax * (float) cos(direcciones.at(i)), vMax * (float) -sin(direcciones.at(i))});
-            sf::Vector2f posicionOvni = ovni.getPosition();
-            // Evita los limites del espacio
-            if (posicionOvni.x < 0) {
-                posicionOvni.x = resolucion.x;
-                ovni.setPosition(posicionOvni);
-            } else if (posicionOvni.x > resolucion.x) {
-                posicionOvni.x = 0;
-                ovni.setPosition(posicionOvni);
-            }
-
-            if (posicionOvni.y < 0) {
-                posicionOvni.y = resolucion.y;
-                ovni.setPosition(posicionOvni);
-            } else if (posicionOvni.y > resolucion.y) {
-                posicionOvni.y = 0;
-                ovni.setPosition(posicionOvni);
-            }
-            for (auto ast = asteroides.begin(); ast != asteroides.end(); ++ast) {
-                if (distanciaEuclidea(ast->getPosicion(), posicionOvni) < ast->getRadio() + ovni.getRadius()) {
-                    // Hay colision, se informa a la red y se reinicia la escena aleatoriamente
-                    choque = true;
-                    break;
-                }
-            }
-            distanciaRecorrida += 3.0f;
-        }
-        if(!choque) {
-            if(i == 0) {
-                direcciones.push_back(valorAleatorio(-1,direcciones.at(i)));
-            } else {
-                direcciones.push_back(valorAleatorio(salidas.at(i-1),salidas.at(i)));
-            }
-        }
-    }
-    if(direcciones.size()==0){
-        direcciones.push_back(direccionObtenida);
-        return direcciones;
-    }
-    return direcciones;
-}
 
 double distancia(sf::Vector2f a, sf::Vector2f b) {
     double distanciaX = (a.x - b.x);
@@ -110,15 +63,40 @@ vector<Asteroide *> asteroideMasCercano(sf::Vector2f posicion) {
     return masCercanos;
 }
 
+void mutacion(vector<double*> estado) {
+    for(unsigned int i = 0; i < estado.size(); i++) {
+        if(valorAleatorio() < 0.01) {
+            *estado[i] = valorAleatorio(-0.5,0.5);
+        }
+    }
+}
+
+void guardarEstado(vector<double*> estado) {
+    for(unsigned int i = 0; i < estado.size(); i++) {
+        estadoAnterior.push_back(*estado[i]);
+    }
+}
+
+void recuperarEstado(vector<double*> estado) {
+    for(unsigned int i = 0; i < estado.size(); i++) {
+        *estado[i] = estadoAnterior[i];
+    }
+}
+
+void combinar(vector<double*> pesos) {
+    int puntoCorte = enteroAleatorio(0,pesos.size());
+    for(unsigned int i = 0; i < puntoCorte+pesos.size()/2; i++) {
+        *pesos[(i+puntoCorte)%pesos.size()] = *pesos[(i+puntoCorte+pesos.size()/2)%pesos.size()];
+        *pesos[(i+puntoCorte+pesos.size()/2)%pesos.size()] = estadoAnterior[(i+puntoCorte)%pesos.size()];
+    }
+}
+
+void entrenarGeneticos(vector<double*> pesos) {
+    combinar(pesos);
+    mutacion(pesos);
+}
+
 int main() {
-// Inicializa una red neuronal nueva
-    /*neural::Network red(4, 1, {120});
-    std::vector<double*> pesos = red.getWeights();
-    cout << *pesos[pesos.size()-1] << endl;
-    *pesos[pesos.size()-1] = 493;
-    cout << *pesos[pesos.size()-1] << endl;
-    std::vector<double*> pesos2 = red.getWeights();
-    cout << *pesos2[pesos2.size()-1] << endl;*/
 
     srand((unsigned long) time(NULL));
     sf::ContextSettings settings;
@@ -132,8 +110,6 @@ int main() {
     sustitutoOvni.setOrigin(sustitutoOvni.getRadius(), sustitutoOvni.getRadius());
     sustitutoOvni.setPosition({rand() % resolucion.x * 1.0f, rand() % resolucion.y * 1.0f});
 
-    // Inicializa una red neuronal nueva
-    neural::Network red(6, 1, {20,20,20});
 
     bool continua = true;
     // Opcionalmente la lee desde fichero
@@ -144,6 +120,8 @@ int main() {
     Asteroide::nuevosAsteroidesAleatorios(asteroides, numAsteroides, resolucion);
     int reinicios = 1;
     unsigned int iteraciones = 0;
+    float tiempoAnterior = -1.0;
+    reloj.restart();
     while (continua) {
         iteraciones++;
         sf::Event event;
@@ -179,8 +157,6 @@ int main() {
 
         double salida = red.run(entradasRed)[0];
         double angulo = 0;
-        vector<double> direcciones = direccionSegura(sustitutoOvni,posicionAnterior,salida);
-        salida = direcciones[0];
         if (salida < -3 * (2.0 / 8.0)) {
             angulo = PI * 3 / 4;
             //cout << "UL ";
@@ -241,16 +217,24 @@ int main() {
             }
         }
         if (choque) {
-            double salidaPre = red.run(entradasRed)[0];
-            red.trainSingle(entradasRed, direccionSegura(sustitutoOvni,posicionAnterior,salida), 0.3);
-            //red.trainSingle(entradasRed, {-1.0*red.run(entradasRed)[0]}, 0.3);
+            float tiempo = reloj.getElapsedTime().asSeconds();
+            vector<double*> pesos = red.getWeights();
+            cout << "Primer peso " << *pesos[0] << " de " << pesos.size() << endl;
+            if(tiempo > tiempoAnterior) {
+                tiempoAnterior = tiempo;
+                guardarEstado(pesos);
+            } else {
+                recuperarEstado(pesos);
+            }
+            entrenarGeneticos(pesos);
+
             Asteroide::nuevosAsteroidesAleatorios(asteroides, numAsteroides, resolucion);
             sustitutoOvni.setPosition({valorAleatorio(0, resolucion.x), valorAleatorio(0, resolucion.y)});
-            //cout << "La salida era: " << salidaPre << " y ahora es: " << red.run(entradasRed)[0] << '\n';
-            cout << reinicios << " " << iteraciones / 60.0f << '\n';
+            cout << reinicios << " " << tiempo << '\n';
             reinicios++;
-           // if (iteraciones / 60.0f > 10) numAsteroides++;
+            // if (iteraciones / 60.0f > 10) numAsteroides++;
             iteraciones = 0;
+            reloj.restart();
         }
         ventana.draw(sustitutoOvni);
         ventana.display();
@@ -258,5 +242,3 @@ int main() {
 
     cout << "Reinicios: " << reinicios << " tiempo: " << reloj.getElapsedTime().asSeconds() << endl;
 }
-
-
